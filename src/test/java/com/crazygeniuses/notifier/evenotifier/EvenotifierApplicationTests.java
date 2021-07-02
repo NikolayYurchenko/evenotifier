377501dd67f7e6fa49f3dc8488fdc0b70b2b9c74
zyurmyk <mykola.yurchenko@ericsson.com>
2021-07-02 16:03:56 +0300

    Initial commit

---
+++ src/test/java/com/crazygeniuses/notifier/evenotifier/EvenotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazygeniuses.notifier.evenotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
